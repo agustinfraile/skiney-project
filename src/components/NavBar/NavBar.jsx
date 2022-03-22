00699fd6529a477e8c1a712413cd8d4e60062425
Agustin Fraile <agustinfraile98@gmail.com>
2022-03-22 02:07:24 -0300

    agregando componente de navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className='navBar-container'>
+
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/TituloApp/TituloApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TituloApp = () => {
+  return (
+    <div>
+        <h1>SKINEY</h1>
+    </div>
+  )
+}
+
+export default TituloApp
